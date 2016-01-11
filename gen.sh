664514ccb23cc7d7e30d0fd87fc7ae1ef49e94df
Manfred Touron <m@42.am>
2016-01-11 17:13:00 +0100

    Create gen.sh

---
+++ gen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#set -x
+set -e
+
+# ensure haveged is running
+
+dir=`mktemp -d`
+GPG=/usr/bin/gpg
+
+function cleanup {
+    set -x
+    rm -rf $dir
+}
+trap cleanup EXIT
+
+
+pushd $dir
+cat stdin | $GPG --batch --gen-key
+$GPG --keyring ./myring.pub --secret-keyring ./myring.sec --list-secret-keys
+KEY=$($GPG --keyring ./myring.pub --secret-keyring ./myring.sec --list-secret-keys | grep sec | cut -d/ -f2 | cut -d' ' -f1 | grep -v myring.sec)
+
+popd
+
+mkdir -p output/$KEY
+mv $dir/myring.* output/$KEY
+echo output/$KEY
